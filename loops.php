b44be5a373d0ec2d7b6953c30b9395a5ed785e6d
Collins1402 <collins.kibiru@strathmore.edu>
2024-05-21 14:03:47 +0300

    commit

---
+++ loops.php
@@ -0,0 +1,32 @@
+<h1>Loops</h1>
+
+<?php
+// while loop
+
+$min = 0;
+while($min < 10){
+    print $min . "<br>";
+    $min++;
+}
+?>
+
+<h4>do-while Loop</h4>
+<?php
+    // do while loop
+    $x = 1;
+    do{
+    print $x . "<br>";
+    $x++;
+   }while($x < 10);
+    
+?>
+<h4> for loop</h4>
+<?php
+    // for loop
+    for($r = 3; r < 12; $r++){
+        print $r . "<br>";
+    }
+
+?>
+
+
